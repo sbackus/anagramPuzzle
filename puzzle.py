5eea2d703afbd0495263a61fbd4d29f59a3b2986
Cayenne <cayennes@gmail.com>
2013-05-02 05:47:20 -0500

    Add comment explaining algorithm

--- puzzle.py
+++ puzzle.py
@@ -4,6 +4,45 @@ def anagramIndex(word):
 	return anagram0Index(word)+1
 
 def anagram0Index(word):
+    # Explanation of algorithm:
+    #
+    # Example: CABA
+    #
+    # AABC
+    # AACB
+    # ABAC
+    # ABCA
+    # ACAB
+    # ACBA
+    # BAAC
+    # BACA
+    # BCAA
+    # CAAB
+    # CABA
+    # CBAA
+    #
+    # To find the index of CABA, we need the sum of:
+    # * the number of # anagrams that start with A
+    # * the number of anagrams that start B, and
+    # * CABA's index within the anagrams that start with C.
+    # The number of anagrams that start with A is the total number of
+    # anagrams of ABC, and the number of anagrams that start with B is
+    # the total number of anagrams of AAC.  CABA's index within the
+    # anagrams that start with C is just ABA's index within its own list
+    # of anagrams.
+    #
+    # More generally: for each letter that's earlier in the alphabet,
+    # add the number of anagrams (permutations) of what's left of the
+    # word without that letter.  Also add the index of the word without
+    # its first letter.
+    #
+    # Complexity: (Assuming the number of different characters and the
+    # length of the list are proportional.) This recurses n times, once
+    # for each character it removes from the list.  It calls a couple
+    # O(n) functions and has an O(n) loop.  O(n * (n + n + n)) = O(n^2),
+    # much better than the O(n!) solution of generating the list of
+    # anagrams.
+
 	# head = word[0]
 	# tail = word[1:]
 	if (len(word) == 1):
